cd0d92c4ba148d991135d88e1540de3d32c59c9b
JakeBadger <jtbadge@g.clemson.edu>
2021-11-04 23:55:27 -0400

    Update SteamData.js

--- SteamData.js
+++ SteamData.js
@@ -1,6 +1,9 @@
-d3.csv("SteamGames.csv").then(function(dataset) {
+d3.csv("SteamGames_Clean.csv").then(function(dataset) {
+
+    console.log(dataset)
+
     var dimensions = {
-        width: 800,
+        width: 1500,
         height: 800,
         margin: {
             top: 10,
@@ -9,22 +12,24 @@ d3.csv("SteamGames.csv").then(function(dataset) {
             left: 50
         }
     }
+
     var svg = d3.select("#scatterplot")
         .style("width", dimensions.width)
         .style("height", dimensions.height)
 
     var xAccessor = d => d.Price
-    var yAccessor = d => d.PerPosAT
+    var yAccessor = d => d.PerPosRevAT
+    var xAccessorNew = d => d.Name
 
     var xScale = d3.scaleLinear()
         .domain(d3.extent(dataset, xAccessor))
         .range([dimensions.margin.left, dimensions.width - dimensions.margin.right])
 
     var yScale = d3.scaleLinear()
-        .domain(d3.extent(dataset,yAccessor))
+        .domain(d3.extent(dataset, yAccessor))
         .range([dimensions.height - dimensions.margin.bottom, dimensions.margin.top])
 
-/*    var dots = svg.selectAll("circle")
+    var dots = svg.selectAll("circle")
         .data(dataset)
         .enter()
         .append("circle")
@@ -32,7 +37,7 @@ d3.csv("SteamGames.csv").then(function(dataset) {
         .attr("cy", d => yScale(yAccessor(d)))
         .attr("fill", "black")
         .attr("r", 3)
-*/
+
     var xAxisgen = d3.axisBottom().scale(xScale)
     var yAxisgen = d3.axisLeft().scale(yScale)
 
