ef7b90452eb9c96a63e467efa7d5f3970557d1a0
JakeBadger <jtbadge@g.clemson.edu>
2021-11-07 15:59:59 -0500

    Update SteamData.js

--- SteamData.js
+++ SteamData.js
@@ -1,5 +1,5 @@
 //scatterplot template
-d3.csv("SteamGames_Test.csv").then(function(dataset) {
+d3.csv("SteamGames_New.csv").then(function(dataset) {
 
     var dimensions = {
         width: 1500,
@@ -56,137 +56,137 @@ d3.csv("SteamGames_Test.csv").then(function(dataset) {
 
 })
 
-//barchart template
-d3.csv("SteamGames_Test.csv").then(function(dataset) {
+// //barchart template
+// d3.csv("SteamGames_Test.csv").then(function(dataset) {
 
-    dimensions = {
-        width: 50000,
-        height: 800,
-        margin: {
-            top: 50,
-            bottom: 50,
-            right: 10,
-            left: 50
-        }
-    }
+//     dimensions = {
+//         width: 50000,
+//         height: 800,
+//         margin: {
+//             top: 50,
+//             bottom: 50,
+//             right: 10,
+//             left: 50
+//         }
+//     }
 
-    var svg = d3.select("#barchart")
-        .style("width", dimensions.width)
-        .style("height", dimensions.height)
+//     var svg = d3.select("#barchart")
+//         .style("width", dimensions.width)
+//         .style("height", dimensions.height)
 
-    var names = dataset.map(d => d.Name)
+//     var names = dataset.map(d => d.Name)
     
-    var xScale = d3.scaleBand()
-        .domain(names) 
-        .range([dimensions.margin.left, dimensions.width - dimensions.margin.right])
-        .padding(0.2)
+//     var xScale = d3.scaleBand()
+//         .domain(names) 
+//         .range([dimensions.margin.left, dimensions.width - dimensions.margin.right])
+//         .padding(0.2)
 
 
-    var yScale = d3.scaleLinear()
-        .domain([0, 100000])
-        .range([dimensions.height - dimensions.margin.bottom, dimensions.margin.top])
+//     var yScale = d3.scaleLinear()
+//         .domain([0, 100000])
+//         .range([dimensions.height - dimensions.margin.bottom, dimensions.margin.top])
 
 
-    var dots = svg.selectAll("rect")
-        .data(dataset)
-        .enter()
-        .append("rect")
-        .attr("x", d => xScale(d.Name))
-        .attr("y", d => yScale(d.TNRAT))
-        .attr("width", xScale.bandwidth())
-        .attr("height", d => dimensions.height - dimensions.margin.top - yScale(d.TNRAT))
-        .attr("fill", "#FF007F");
+//     var dots = svg.selectAll("rect")
+//         .data(dataset)
+//         .enter()
+//         .append("rect")
+//         .attr("x", d => xScale(d.Name))
+//         .attr("y", d => yScale(d.TNRAT))
+//         .attr("width", xScale.bandwidth())
+//         .attr("height", d => dimensions.height - dimensions.margin.top - yScale(d.TNRAT))
+//         .attr("fill", "#FF007F");
 
 
-    var xAxisgen = d3.axisBottom().scale(xScale)
-    var yAxisgen = d3.axisLeft().scale(yScale)
+//     var xAxisgen = d3.axisBottom().scale(xScale)
+//     var yAxisgen = d3.axisLeft().scale(yScale)
 
 
-    var xAxis = svg.append("g")
-        .call(xAxisgen)
-        .style("transform", `translateY(${dimensions.height - dimensions.margin.bottom}px)`) 
-        .selectAll("text")
-        .attr("y", 0)
-        .attr("x", "-15em")
-        .attr("transform", "rotate(-65)")
-        .style("font-size", "6px")
+//     var xAxis = svg.append("g")
+//         .call(xAxisgen)
+//         .style("transform", `translateY(${dimensions.height - dimensions.margin.bottom}px)`) 
+//         .selectAll("text")
+//         .attr("y", 0)
+//         .attr("x", "-15em")
+//         .attr("transform", "rotate(-65)")
+//         .style("font-size", "6px")
 
 
-    var yAxis = svg.append("g")
-        .call(yAxisgen.ticks(22))
-        .style("transform", `translateX(${dimensions.margin.left}px)`)
+//     var yAxis = svg.append("g")
+//         .call(yAxisgen.ticks(22))
+//         .style("transform", `translateX(${dimensions.margin.left}px)`)
 
 
-    svg.append("text")
-        .attr("x", (dimensions.width / 2))             
-        .attr("y", 20)
-        .attr("text-anchor", "middle")  
-        .style("font-size", "24px") 
-        .style("text-decoration", "underline")  
-        .text("Steam Bar Graph");
+//     svg.append("text")
+//         .attr("x", (dimensions.width / 2))             
+//         .attr("y", 20)
+//         .attr("text-anchor", "middle")  
+//         .style("font-size", "24px") 
+//         .style("text-decoration", "underline")  
+//         .text("Steam Bar Graph");
 
-})
+// })
 
-//forces template
-d3.csv("SteamGames_Test.csv").then(function(dataset) {
+// //forces template
+// d3.csv("SteamGames_Test.csv").then(function(dataset) {
 
-    console.log(dataset)
+//     console.log(dataset)
 
-    var dimensions = {
-        width: 1500,
-        height: 800,
-        margin: {
-            top: 10,
-            bottom: 50,
-            right: 10,
-            left: 50
-        }
-    }
+//     var dimensions = {
+//         width: 1500,
+//         height: 800,
+//         margin: {
+//             top: 10,
+//             bottom: 50,
+//             right: 10,
+//             left: 50
+//         }
+//     }
 
-    var colors = dataset.map(d => d.Name)
-    console.log(colors)
+//     var colors = dataset.map(d => d.Name)
+//     console.log(colors)
     
-    var color = d3.scaleOrdinal()
-        .domain(colors)
-        .range(d3.schemeCategory10)
+//     var color = d3.scaleOrdinal()
+//         .domain(colors)
+//         .range(d3.schemeCategory10)
 
-    var svg = d3.select("#barchart1")
-        .style("width", dimensions.width)
-        .style("height", dimensions.height)
+//     var svg = d3.select("#barchart1")
+//         .style("width", dimensions.width)
+//         .style("height", dimensions.height)
 
-    var nodes = dataset.map(function(d) {
-        return {
-            Developer: d.Developer,
-            ID: d.ID
-        }
-    });
-    console.log(nodes)
+//     var nodes = dataset.map(function(d) {
+//         return {
+//             Developer: d.Developer,
+//             ID: d.ID
+//         }
+//     });
+//     console.log(nodes)
 
-    var layout = d3.forceSimulation(nodes)
-        .force('center', d3.forceCenter(dimensions.width/2, dimensions.height/2))
-        .force('collisions', d3.forceCollide().radius(function(d) {
-          return d.TNRAT
-        }))
-        .on('tick', ticked)
+//     var layout = d3.forceSimulation(nodes)
+//         .force('center', d3.forceCenter(dimensions.width/2, dimensions.height/2))
+//         .force('collisions', d3.forceCollide().radius(function(d) {
+//           return d.TNRAT
+//         }))
+//         .on('tick', ticked)
 
-    let node = svg.append("g")
-        .selectAll("circle")
-        .data(nodes).enter()
-        .append("circle")
-        .attr('cx', d => d.x)
-        .attr('cy', d => d.y)
-        .attr("fill", d => color(d.Developer))
-        .attr("opacity", 0.4)
-        .attr("r", d => d.TNRAT)
+//     let node = svg.append("g")
+//         .selectAll("circle")
+//         .data(nodes).enter()
+//         .append("circle")
+//         .attr('cx', d => d.x)
+//         .attr('cy', d => d.y)
+//         .attr("fill", d => color(d.Developer))
+//         .attr("opacity", 0.4)
+//         .attr("r", d => d.TNRAT)
       
-    function ticked(){
-        svg.selectAll("circle")
-            .attr('cx', d => d.x)
-            .attr('cy', d => d.y)
-    }
-})
+//     function ticked(){
+//         svg.selectAll("circle")
+//             .attr('cx', d => d.x)
+//             .attr('cy', d => d.y)
+//     }
+// })
 
-//heatmap template
+// //heatmap template
 // d3.csv("SteamGames_Test.csv").then(function(dataset) {
 
 //     console.log(dataset)
@@ -243,7 +243,7 @@ d3.csv("SteamGames_Test.csv").then(function(dataset) {
 //     //     .style("fill", function(d) { return myColor(d.value)} )
 // })
 
-// //I want to try something here
+// // //I want to try something here
 // d3.csv("SteamGames_Test.csv").then(function(dataset) {
 
 //     console.log(dataset)
@@ -303,7 +303,7 @@ d3.csv("SteamGames_Test.csv").then(function(dataset) {
 //         .style("fill", d => color(d.Developer))
 // })
 
-// //Memory Size and Price
+// // //Memory Size and Price
 // d3.csv("SteamGames_Test.csv").then(function(dataset) {
 
 //     console.log(dataset)
