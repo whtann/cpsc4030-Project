df6233aa7355b4a185a48768df698429f41f12e9
JakeBadger <jtbadge@g.clemson.edu>
2021-11-08 00:48:40 -0500

    Update svgswap.js

--- svgswap.js
+++ svgswap.js
@@ -2,9 +2,81 @@ function switchVisible(graph) {
     if (graph == "sp") {
         document.getElementById('scatterplot').style.display = 'block';
         document.getElementById('barchart').style.display = 'none';
+        document.getElementById('areachart').style.display = 'none';
+        document.getElementById('barchart2').style.display = 'none';
+        document.getElementById('scatterplot2').style.display = 'none';
+        document.getElementById('scatterplot3').style.display = 'none';
+        document.getElementById('areachart2').style.display = 'none';
+        document.getElementById('areachart3').style.display = 'none';
     }
     else if (graph == "bar") {
         document.getElementById('scatterplot').style.display = 'none';
         document.getElementById('barchart').style.display = 'block';
+        document.getElementById('areachart').style.display = 'none';
+        document.getElementById('barchart2').style.display = 'none';
+        document.getElementById('scatterplot2').style.display = 'none';
+        document.getElementById('scatterplot3').style.display = 'none';
+        document.getElementById('areachart2').style.display = 'none';
+        document.getElementById('areachart3').style.display = 'none';
+    }
+    if (graph == "sp") {
+        document.getElementById('scatterplot').style.display = 'none';
+        document.getElementById('barchart').style.display = 'none';
+        document.getElementById('areachart').style.display = 'block';
+        document.getElementById('barchart2').style.display = 'none';
+        document.getElementById('scatterplot2').style.display = 'none';
+        document.getElementById('scatterplot3').style.display = 'none';
+        document.getElementById('areachart2').style.display = 'none';
+        document.getElementById('areachart3').style.display = 'none';
+    }
+    else if (graph == "bar") {
+        document.getElementById('scatterplot').style.display = 'none';
+        document.getElementById('barchart').style.display = 'none';
+        document.getElementById('areachart').style.display = 'none';
+        document.getElementById('barchart2').style.display = 'block';
+        document.getElementById('scatterplot2').style.display = 'none';
+        document.getElementById('scatterplot3').style.display = 'none';
+        document.getElementById('areachart2').style.display = 'none';
+        document.getElementById('areachart3').style.display = 'none';
+    }
+    if (graph == "sp") {
+        document.getElementById('scatterplot').style.display = 'none';
+        document.getElementById('barchart').style.display = 'none';
+        document.getElementById('areachart').style.display = 'none';
+        document.getElementById('barchart2').style.display = 'none';
+        document.getElementById('scatterplot2').style.display = 'block';
+        document.getElementById('scatterplot3').style.display = 'none';
+        document.getElementById('areachart2').style.display = 'none';
+        document.getElementById('areachart3').style.display = 'none';
+    }
+    else if (graph == "bar") {
+        document.getElementById('scatterplot').style.display = 'none';
+        document.getElementById('barchart').style.display = 'none';
+        document.getElementById('areachart').style.display = 'none';
+        document.getElementById('barchart2').style.display = 'none';
+        document.getElementById('scatterplot2').style.display = 'none';
+        document.getElementById('scatterplot3').style.display = 'block';
+        document.getElementById('areachart2').style.display = 'none';
+        document.getElementById('areachart3').style.display = 'none';
+    }
+    if (graph == "sp") {
+        document.getElementById('scatterplot').style.display = 'none';
+        document.getElementById('barchart').style.display = 'none';
+        document.getElementById('areachart').style.display = 'none';
+        document.getElementById('barchart2').style.display = 'none';
+        document.getElementById('scatterplot2').style.display = 'none';
+        document.getElementById('scatterplot3').style.display = 'none';
+        document.getElementById('areachart2').style.display = 'block';
+        document.getElementById('areachart3').style.display = 'none';
+    }
+    else if (graph == "bar") {
+        document.getElementById('scatterplot').style.display = 'none';
+        document.getElementById('barchart').style.display = 'none';
+        document.getElementById('areachart').style.display = 'none';
+        document.getElementById('barchart2').style.display = 'none';
+        document.getElementById('scatterplot2').style.display = 'none';
+        document.getElementById('scatterplot3').style.display = 'none';
+        document.getElementById('areachart2').style.display = 'none';
+        document.getElementById('areachart3').style.display = 'block';
     }
 }
