d179a2762214baca77c99774f1c9ce1c89c07b5f
JakeBadger <jtbadge@g.clemson.edu>
2021-11-29 10:59:47 -0500

    Update SteamData2.js

--- SteamData2.js
+++ SteamData2.js
@@ -225,7 +225,7 @@ d3.csv("SteamGamesLarger.csv").then(function(dataset) {
         .attr("y", d => yScale(d.AvgNRAT))
         .attr("width", xScale.bandwidth())
         .attr("height", d => dimensions.height - dimensions.margin.bottom - yScale(d.AvgNRAT))
-        .attr("fill", "blue")//d => color(d.Genre)) FF007F
+        .attr("fill", "orange")//d => color(d.Genre)) FF007F
         .on('mouseover', function(d, i){
             d3.select(this)
                 .attr("stroke-width", 1)
@@ -345,6 +345,32 @@ d3.csv("SteamGamesLarger.csv").then(function(dataset) {
             .text("Average Price per Genre of Game");
     })
 
+    d3.select("#AvgCapacity").on('click', function(){
+
+        yScale.domain([0, d3.max(dataset.map(function(d){return d["AvgStore"]}), s => +s)])
+            .range([dimensions.height - dimensions.margin.bottom, dimensions.margin.top])
+
+        //yAxisgen = d3.axisLeft().scale(yScale)
+            
+        yAxis.call(yAxisgen.ticks(22))
+            .style("transform", `translateX(${dimensions.margin.left}px)`)
+
+        changing_axis.transition()//.call(yAxis)
+
+        dots.transition()
+            .attr('x', function(d) { return xScale(d.Genre); })
+            .attr('width', xScale.bandwidth)
+            .attr('y', function(d) { return yScale(d["AvgStore"]); })
+            .attr('height', function(d){return dimensions.height - dimensions.margin.bottom - yScale(d["AvgStore"])})
+            .duration(1000)
+
+        text.attr("x", (dimensions.width / 2))             
+            .attr("y", 20)
+            .attr("text-anchor", "middle")  
+            .style("font-size", "24px") 
+            .style("text-decoration", "underline")  
+            .text("Average Storage Requirement per Genre of Game");
+    })
 
 })
 
@@ -362,7 +388,7 @@ d3.csv("SteamGamesLarger.csv").then(function(dataset) {
 //         }
 //     }
 
-//     var colors = dataset.map(d => d.ID)
+//     var colors = dataset.map(d => d.Genre)
     
 //     var color = d3.scaleOrdinal()
 //         .domain(colors)
