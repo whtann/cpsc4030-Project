320d04305907a3ff0eb7ca25b9fcefd2d0f95bc0
William Harrison Tann <harrisontann@gmail.com>
2021-12-04 15:59:14 -0500

    not working
    This reverts commit c5361b17846f32c3c532877edc82488a471f8589.

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # cpsc4030-Project
+http://ec2-3-212-192-153.compute-1.amazonaws.com/
 
 https://whtann.github.io/cpsc4030-Project/
 
--- SteamData2.js
+++ SteamData2.js
@@ -1,6 +1,6 @@
 var scatterDiv = document.getElementById("scatterplot");
 var barDiv = document.getElementById("barchart");
-var areaDiv = document.getElementById("areachart");
+var heatDiv = document.getElementById("heatmap");
 
 var newData = [];
 
@@ -407,11 +407,11 @@ d3.csv("SteamGamesLarger.csv").then(function(dataset) {
     })
 })
 
-d3.csv("SteamGamesLarger.csv").then(function(dataset) {
+d3.csv("HeatMap.csv").then(function(dataset) {
     //code for forces
-    var areaDimensions = {
-        width: areaDiv.clientWidth,
-        height: areaDiv.clientHeight,
+    var heatDimensions = {
+        width: heatDiv.clientWidth,
+        height: heatDiv.clientHeight,
         margin: {
             top: 50,
             bottom: 50,
@@ -419,75 +419,113 @@ d3.csv("SteamGamesLarger.csv").then(function(dataset) {
             left: 50
         }
     }
-
-    var areaColors = d3.scaleOrdinal()
-    .domain(["Mac", "Windows", "SteamLinux"])
-    .range(d3.schemeCategory10)
-
-    var svg = d3.select("#areachart")
-        .style("width", areaDimensions.width)
-        .style("height", areaDimensions.height)
+    console.log(heatDiv.clientHeight)
+    // append the svg object to the body of the page
+    var svg = d3.select("#heatmap")
+        .style("width", heatDimensions.width)
+        .style("height", heatDimensions.height)
         .append("svg")
-            .attr("width", areaDimensions.width)
-            .attr("height", areaDimensions.height)
+            .attr("width", heatDimensions.width)
+            .attr("height", heatDimensions.height)
 
-    var text = svg
-            .append('text')
-            .attr("id", 'topbartext')
-            .attr("x", 300)
-            .attr("y", 20)
-            .attr("dx", "-.8em")
-            .attr("dy", ".15em")
-            .attr("font-family", "sans-serif")
-            .text("Green = Linux; Orange = Windows; Blue = Mac")
+    // Labels of row and columns -> unique identifier of the column called 'group' and 'variable'
+    var myGroups = d3.map(dataset, function(d){return d.Price})
+    var myVars = d3.map(dataset, function(d){return d.Genre})
 
-    var nodes = [{r: newData.Mac, i: "Mac"}, {r: newData.Windows, i: "Windows"}, {r: newData.SteamLinux, i: "SteamLinux"}]
-    console.log(nodes)
+    // Build X scales and axis:
+    var x = d3.scaleBand()
+    .range([0, heatDimensions.width])
+    .domain(myGroups)
+    .padding(0.05);
+    svg.append("g")
+    .style("font-size", 15)
+    .attr("transform", "translate(0," + heatDimensions.height + ")")
+    .call(d3.axisBottom(x).tickSize(0))
+    .select(".domain").remove()
 
-    var layout = d3.forceSimulation(nodes)
-        .force('center', d3.forceCenter(areaDimensions.width/2, areaDimensions.height/2))
-        .force('collisions', d3.forceCollide().radius(function(d) {return d => d.r}))
-        .force('charge', d3.forceManyBody().strength(-30))
-        .on('tick', ticked)
+    // Build Y scales and axis:
+    var y = d3.scaleBand()
+    .range([heatDimensions.height, 0 ])
+    .domain(myVars)
+    .padding(0.05);
+    svg.append("g")
+    .style("font-size", 15)
+    .call(d3.axisLeft(y).tickSize(0))
+    .select(".domain").remove()
 
-    var node = svg.append("g")
-        .selectAll("circle")
-        .data(nodes).enter()
-        .append("circle")
-        .attr('cx', d => d.x)
-        .attr('cy', d => d.y)
-        .attr("fill", d => areaColors(d.i))
-        .attr("opacity", .5)
-        .attr("r", d => d.r * 50)
-        
-    function ticked(){
-        svg.selectAll("circle")
-            .attr('cx', d => d.x)
-            .attr('cy', d => d.y)
+    // Build color scale
+    var myColor = d3.scaleSequential()
+    .interpolator(d3.interpolateYlOrRd)
+    .domain([1,100])
+
+    // create a tooltip
+    var tooltip = d3.select("#scatterplot")
+    .append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
+    .style("background-color", "white")
+    .style("border", "solid")
+    .style("border-width", "2px")
+    .style("border-radius", "5px")
+    .style("padding", "5px")
+
+    //Three function that change the tooltip when user hover / move / leave a cell
+    var mouseover = function(d) {
+    tooltip
+        .style("opacity", 1)
+    d3.select(this)
+        .style("stroke", "red")
+        .style("opacity", 1)
+    }
+    var mousemove = function(event, d) {
+    tooltip
+        .html("The number of games in<br>this price range is: " +d.Value)
+        .style("left", (event.x)/2 + "px")
+        .style("top", (event.y)/2 + "px")
+    }
+    var mouseleave = function(d) {
+    tooltip
+        .style("opacity", 0)
+    d3.select(this)
+        .style("stroke", "none")
+        .style("opacity", 0.8)
     }
 
-    d3.select("#scatterplot").on('click', function() {
-        d3.select("#areachart")
-        .selectAll("circle")
-        .remove();
+    // add the squares
+    svg.selectAll("rect")
+    .data(dataset, function(d) {return d.Genre+':'+d.Price;})
+    .enter()
+    .append("rect")
+        .attr("x", function(d) {return x(d.Price) })
+        .attr("y", function(d) {return y(d.Genre) })
+        .attr("rx", 4)
+        .attr("ry", 4)
+        .attr("width", x.bandwidth() )
+        .attr("height", y.bandwidth() )
+        .style("fill", function(d) {return myColor(d.Value)})
+        .style("stroke-width", 4)
+        .style("stroke", "none")
+        .style("opacity", 0.8)
+    .on("mouseover", mouseover)
+    .on("mousemove", mousemove)
+    .on("mouseleave", mouseleave)
 
-        nodes = [{r: newData.Mac, i: "Mac"}, {r: newData.Windows, i: "Windows"}, {r: newData.SteamLinux, i: "SteamLinux"}]
-        console.log(nodes)
 
-        var layout = d3.forceSimulation(nodes)
-        .force('center', d3.forceCenter(areaDimensions.width/2, areaDimensions.height/2))
-        .force('collisions', d3.forceCollide().radius(function(d) {return d => d.r}))
-        .force('charge', d3.forceManyBody().strength(-30))
-        .on('tick', ticked)
+    // Add title to graph
+    svg.append("text")
+        .attr("x", 0)
+        .attr("y", -50)
+        .attr("text-anchor", "left")
+        .style("font-size", "22px")
+        .text("A Heatmap of Games ");
 
-        var node = svg.append("g")
-        .selectAll("circle")
-        .data(nodes).enter()
-        .append("circle")
-        .attr('cx', d => d.x)
-        .attr('cy', d => d.y)
-        .attr("fill", d => areaColors(d.i))
-        .attr("opacity", .5)
-        .attr("r", d => d.r * 50)
-    })
+    // Add subtitle to graph
+    svg.append("text")
+        .attr("x", 0)
+        .attr("y", -20)
+        .attr("text-anchor", "left")
+        .style("font-size", "14px")
+        .style("fill", "grey")
+        .style("max-width", 400)
+        .text("A short description of the take-away message of this chart.");
 })
